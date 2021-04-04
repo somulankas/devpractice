92119250c09c8c8b4ed1cc8019dd5a5b0e5d0431
Somulankas <somu2k7@gmail.com>
2021-04-04 09:39:13 +0530

    new changes in master

--- Hello.java
+++ Hello.java
@@ -1,11 +1,2 @@
-class Hello
-{
-	public static void main(String args[])
-	{
-		System.out.println("Hello World");
-	}
- Mera bharath Mahan hain
 
-}
-
-Child IP address-10.10.2.1
+master@Hellow master data
